d835140a406435e6bd899e3f1c53ab7cb99818f3
BenMorrow03 <ben.morrow03@gmail.com>
2026-01-15 00:23:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const { app } = require('@azure/functions');
 
+
+require("./src/functions/CreateMedia");
+require("./src/functions/GetMedia");
+require("./src/functions/UpdateMedia");
+require("./src/functions/DeleteMedia");
+require("./src/functions/HelloFunction");
